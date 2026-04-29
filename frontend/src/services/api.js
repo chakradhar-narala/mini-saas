40c223b68691b4ceec66c9268a8cbb5305710e01
chakradhar.narala <naralachakradharreddy@gmail.com>
2026-04-29 11:50:05 +0530

    updation of JWT Secret key

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mini-saas-4rgb.onrender.com",
+  baseURL: "https://mini-saas-4rgb.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
